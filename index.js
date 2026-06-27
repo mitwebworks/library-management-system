0df8bdce16e7402716fc192f455e208a335f8b91
mitwebwoks <mitwebworks@gmail.com>
2026-06-27 07:47:22 +0530

    Routes added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-const { users } = require("./data/users.json");
+// importing routes
+const usersRouter = require("./routes/users");
+// const booksRouter = require('./routes/books')
 
 app.use(express.json());
 
@@ -13,115 +15,7 @@ app.get("/", (req, res) => {
   });
 });
 
-/**
- * Route: /users
- * Method: GET
- * Description: Get the list of all users in the system
- * Access: Public
- * Parameters: None
- */
-app.get("/users", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: users,
-  });
-});
-
-/**
- * Route: /users/:id
- * Method: GET
- * Description: Get the details of the user by id
- * Access: Public
- * Parameters: id
- */
-app.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const user = users.find((each) => each.id === id);
-
-  if (!user) {
-    return res.status(400).json({
-      success: false,
-      message: `User not found for id: ${id}`,
-    });
-  }
-
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
-
-/**
- * Route: /users
- * Method: POST
- * Description: Create a new user
- * Access: Public
- * Parameters: None
- */
-app.post("/users", (req, res) => {
-  const { id, name, email, role, borrowedBooks } = req.body;
-
-  if (!id || !name || !role) {
-    return res.status(400).json({
-      success: false,
-      message: "Please provide all details",
-    });
-  }
-
-  const user = users.find((each) => each.id === id);
-  if (user) {
-    return res.status(409).json({
-      success: false,
-      message: `User already exists for id: ${id}`,
-    });
-  }
-
-  users.push({
-    id,
-    name,
-    email,
-    role,
-    borrowedBooks,
-  });
-
-  res.status(201).json({
-    success: true,
-    message: "User added successfully!",
-  });
-});
-
-/**
- * Route: /users
- * Method: PUT
- * Description: Update a user
- * Access: Public
- * Parameters: id
- */
-app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-
-  // check if user exists
-  const user = users.find((each) => each.id === id);
-  if (!user) {
-    return res.status(400).json({
-      success: false,
-      message: `User not found for id: ${id}`,
-    });
-  }
-
-  const updatedUser = users.map((each) => {
-    if (each.id === id) {
-      return { ...each, ...data };
-    }
-    return each;
-  });
-
-  res.send(200).json({
-    success: true,
-    message: "User updated successfully!",
-  });
-});
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port} `);
---
+++ routes/users.js
@@ -0,0 +1,137 @@
+const express = require("express");
+const { users } = require("../data/users.json");
+
+const router = express.Router();
+
+/**
+ * Route: /users
+ * Method: GET
+ * Description: Get the list of all users in the system
+ * Access: Public
+ * Parameters: None
+ */
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: users,
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method: GET
+ * Description: Get the details of the user by id
+ * Access: Public
+ * Parameters: id
+ */
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id === id);
+
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      message: `User not found for id: ${id}`,
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+/**
+ * Route: /users
+ * Method: POST
+ * Description: Create a new user
+ * Access: Public
+ * Parameters: None
+ */
+app.post("/", (req, res) => {
+  const { id, name, email, role, borrowedBooks } = req.body;
+
+  if (!id || !name || !role) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all details",
+    });
+  }
+
+  const user = users.find((each) => each.id === id);
+  if (user) {
+    return res.status(409).json({
+      success: false,
+      message: `User already exists for id: ${id}`,
+    });
+  }
+
+  users.push({
+    id,
+    name,
+    email,
+    role,
+    borrowedBooks,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "User added successfully!",
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method: PUT
+ * Description: Update a user
+ * Access: Public
+ * Parameters: id
+ */
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+
+  // check if user exists
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      message: `User not found for id: ${id}`,
+    });
+  }
+
+  const updatedUser = users.map((each) => {
+    if (each.id === id) {
+      return { ...each, ...data };
+    }
+    return each;
+  });
+
+  res.send(200).json({
+    success: true,
+    message: "User updated successfully!",
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method: DELETE
+ * Description: Delete a user
+ * Access: Public
+ * Parameters: id
+ */
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = user.find((each) => each.id === id);
+
+  if (!user) {
+    return res.send(400).json({
+      success: false,
+      message: `User not found for id: ${id}`,
+    });
+  }
+
+  // Delete user from array
+});
+
+module.exports = router;
